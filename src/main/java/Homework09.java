10e7d4cc56b876997f0b1a1e781c67618e3e6a61
Josh <125091065+joshxviii@users.noreply.github.com>
2024-04-12 22:30:37 -0400

    updated comments

--- src/main/java/Homework09.java
+++ src/main/java/Homework09.java
@@ -1,38 +1,37 @@
-
-/**
+/* *
  * @author Joshua Sherry
- */
-public class Homework09 {
+ * */
 
+public class Homework09  {
     public static void main(String[] args) {
-        findTriangleNumByFactors(100);
+        findTriangleNumByFactors(10);
     }
 
     public static int findTriangleNumByFactors(int factorAmount) {
         int factorCount = 0;
-        int i=0;
-        while(factorCount<=factorAmount) {
+        int i = 0;
+        while (factorCount <= factorAmount) {
             i++;
             factorCount = countFactors(triangleNum(i));
         }
-        System.out.println("\n" + triangleNum(i) + " is the first triangle number that has "+ factorAmount +" or more factors. " + "("+factorCount+")");
+        System.out.println("\n" + triangleNum(i) + " is the first triangle number that has " + factorAmount + " or more factors. " + "(" + factorCount + ")");
         return triangleNum(i);
     } // use 'triangleNum' as an argument for the 'countFactors' method and keep iterating until 'factorCount' surpasses the target 'factorAmount' (100).
 
     public static int triangleNum(int num) {
-        return (num*(num+1))/2;
+        return (num * (num + 1)) / 2;
     } // formula for calculating triangle numbers.
 
     public static int countFactors(int num) {
         int count = 0;
-        //System.out.print(num+": ");
+        System.out.print(num + ": ");
         for (int i = 1; i <= num; i++) {
             if (num % i == 0) {
-                //System.out.print(i+" ");
+                System.out.print(i + " ");
                 count++;
             }
         }
-        //System.out.print("\n");
+        System.out.print("\n");
         return count;
     } // iterate from 1 to n and count how many numbers evenly divide in the target 'num'.
-}
+ }
